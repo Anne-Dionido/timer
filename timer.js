c0a4dec393a2ecd9b91c223b9cb1ba8d6e6ea60f
Anne-Dionido <anne.dionido@telus.com>
2022-11-23 15:21:11 -0700

    updated final

---
+++ timer.js
@@ -0,0 +1,33 @@
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Please enter alarm seconds.", numbers => {
+
+  let timer = 0;
+  let timerSum = 0;
+
+  let arr = numbers.split(' ');
+  if (arr.length === 0) {
+    rl.close();
+  }
+
+  arr = arr.filter(word => word.length !== 0);
+  for (let str of arr) {
+
+    if (!isNaN(str)) {
+
+      timer = Number(str) * 1000;
+
+      if (timer >= 0) {
+        timerSum += timer;
+        setTimeout(() => {
+          process.stdout.write('\x07');
+        }, timerSum);
+      }
+
+    }
+  }
+  rl.close();
+});
